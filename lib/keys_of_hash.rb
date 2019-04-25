9bb96155af3f00f9bb6242946aeeed11d81fbcf7
Martin Spicuzza <spicuzza157@gmail.com>
2019-04-25 19:14:00 +0000

    Done.

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,24 @@
 class Hash
-  def keys_of(arguments)
+  def keys_of(*arguments)
     # code goes here
+    
+    arr = []
+    #puts "#{arguments} are the args"
+    self.each do |k , v|
+       # arr << k
+        arguments.each do |i|
+          puts i
+          if i == v
+            arr << k
+          end
+        end
+      #  puts "#{arguments} is arg , #{v} is the passed val"
+      #  if arguments == v
+      #    puts "Found something to push yo"
+      #  end
+    end
+    
+    puts "#{arr} is the result"
+    return arr
   end
 end
